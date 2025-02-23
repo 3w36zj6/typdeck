e805b5a10fe4949afe1ed6a842c06043675020b2
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2025-02-23 20:06:45 +0900

    feat: integrate dot directory setup into config creation

--- src/server/helpers/vite/createConfig.ts
+++ src/server/helpers/vite/createConfig.ts
@@ -1,8 +1,41 @@
 import { InlineConfig } from "vite";
 import path from "path";
+import fs from "fs";
 import { typstPlugin } from "./createTypstPlugin";
-import { copyPublicFilesPlugin } from "./createCopyPublicFilesPlugin";
 import { injectIndexHtmlPlugin } from "./createInjectIndexHtmlPlugin";
+import { getPackageRootPath } from "../getPackageRootPath";
+import { compileTypst } from "./compileTypst";
+
+const setupDotDirectory = (typstFile?: string) => {
+  const packageRoot = getPackageRootPath();
+  const sourcePublicDir = path.join(packageRoot, "public");
+  const destPublicDir = path.resolve(process.cwd(), ".typdeck/public");
+  const destDotFolderDir = path.resolve(process.cwd(), ".typdeck");
+
+  try {
+    if (fs.existsSync(sourcePublicDir)) {
+      fs.cpSync(sourcePublicDir, destPublicDir, {
+        recursive: true,
+        force: true,
+      });
+    } else {
+      console.warn(
+        `Source public directory ${sourcePublicDir} does not exist.`,
+      );
+    }
+    const gitignoreContent = `*
+`;
+
+    const destGitignorePath = path.join(destDotFolderDir, ".gitignore");
+    fs.writeFileSync(destGitignorePath, gitignoreContent, "utf-8");
+
+    if (typstFile) {
+      compileTypst(typstFile, ".typdeck/public");
+    }
+  } catch (err) {
+    console.error("Failed to copy public files:", err);
+  }
+};
 
 type CommandType = "dev" | "build" | "preview";
 
@@ -10,20 +43,22 @@ export const createConfig = (
   outDir: string,
   typstFile?: string,
   command?: CommandType,
-): InlineConfig => ({
-  configFile: false,
-  root:
-    command !== "build"
-      ? process.cwd()
-      : path.join(process.cwd(), ".typdeck/public"),
-  publicDir: path.join(process.cwd(), ".typdeck/public"),
-  plugins: [
-    copyPublicFilesPlugin(),
-    injectIndexHtmlPlugin(),
-    ...(typstFile ? [typstPlugin(typstFile)] : []),
-  ],
-  build: {
-    outDir: path.resolve(process.cwd(), outDir),
-    emptyOutDir: true,
-  },
-});
+): InlineConfig => {
+  setupDotDirectory(typstFile);
+  return {
+    configFile: false,
+    root:
+      command !== "build"
+        ? process.cwd()
+        : path.join(process.cwd(), ".typdeck/public"),
+    publicDir: path.join(process.cwd(), ".typdeck/public"),
+    plugins: [
+      injectIndexHtmlPlugin(),
+      ...(typstFile ? [typstPlugin(typstFile)] : []),
+    ],
+    build: {
+      outDir: path.resolve(process.cwd(), outDir),
+      emptyOutDir: true,
+    },
+  };
+};
